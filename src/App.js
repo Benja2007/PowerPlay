1940db108f3afedce08074c919cca38e27b48c68
Benja2007 <130391949+Benja2007@users.noreply.github.com>
2023-11-07 14:39:06 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>PowerPlay</h1>
+      <div className="search-bar">
+        <input type="text" placeholder="Buscar videojuegos" />
+        <button type="submit">Buscar</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
